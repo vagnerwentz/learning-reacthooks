561619079b7fc762e551a28864caeac2bc6eac59
vagnerwentz <wentz.vagner@gmail.com>
2020-04-13 19:20:18 -0300

    Configured React Hooks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,41 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'plugin:react/recommended',
-      'airbnb',
-      'prettier',
-      'prettier/react'
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parser: 'babel-eslint',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-      'prettier'
-    ],
-    rules: {
-        'prettier/prettier': 'error',
-        'react/jsx-filename-extension': [
-            'warn',
-            { extensions: ['.jsx', '.js'] }
-        ],
-        'import/prefer-default-export': 'off'
-    },
-  };
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+};
